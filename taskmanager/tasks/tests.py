d823ac140a3c2388005c2547dfe941a8db5d4618
Seth Nkwo <sethnkwocool@gmail.com>
2025-11-08 15:59:10 +0100

    Completed all APITestCases

--- taskmanager/tasks/tests.py
+++ taskmanager/tasks/tests.py
@@ -12,7 +12,7 @@ User = get_user_model()
 # Create your tests here.
 class BaseAPITestCase(APITestCase):
     def setUp(self):
-        self.user = User.objects.create(
+        self.user = User.objects.create_user(
             username='testuser',
             email='test@example.com',
             password='password123'
@@ -31,9 +31,35 @@ class TaskAPITestCase(BaseAPITestCase):
                                         completed=False)
         self.task_url= reverse('tasks:tasks-list')
         self.detail_url= reverse('tasks:tasks-detail', kwargs={'pk':self.task.id})
+        self.task_data = {'title': 'test2', 'description':'Sample data', 'priority': 'low', 'completed': True}
 
     def test_get_tasks(self):
         response = self.client.get(self.task_url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['count'], 1)
         self.assertEqual(len(response.data['results']), 1)
+
+    def test_create_task(self):
+        response = self.client.post(self.task_url, self.task_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['title'], 'test2')
+
+    def test_retrieve_task(self):
+        response = self.client.get(self.detail_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['title'], 'Complete test')
+
+    def test_put_task(self):
+        response = self.client.put(self.detail_url, self.task_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['title'], 'test2')
+
+    def test_delete_task(self):
+        response = self.client.delete(self.detail_url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_unauthorized(self):
+        self.client.credentials()
+        response = self.client.get(self.task_url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
